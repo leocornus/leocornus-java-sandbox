6cda21e4c3f78c1f5be269670d9e5b74db050070
Sean Chen <sean.chen@leocorn.com>
2018-11-26 18:08:04 +0000

    Spring boot standard alone java application, generated by using spring initializr https://start.spring.io

---
+++ adventures/azure/src/main/java/com/leocorn/sandbox/azure/AzureApplication.java
@@ -0,0 +1,12 @@
+package com.leocorn.sandbox.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureApplication.class, args);
+	}
+}
