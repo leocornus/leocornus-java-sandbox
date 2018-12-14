e67801c457cd95aee4b837d809313664bca5311f
Sean Chen <sean.chen@leocorn.com>
2018-12-14 19:14:33 +0000

    test to calculate file size and generate the MD5 hash

--- adventures/maven-basic/basic-tasks/src/test/java/com/leocorn/sandbox/stream/InputStreamTest.java
+++ adventures/maven-basic/basic-tasks/src/test/java/com/leocorn/sandbox/stream/InputStreamTest.java
@@ -2,6 +2,8 @@ package com.leocorn.sandbox.stream;
 
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 
 import java.util.Properties;
@@ -12,6 +14,8 @@ import java.net.HttpURLConnection;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  * test for the properties util class.
@@ -72,6 +76,44 @@ public class InputStreamTest extends TestCase {
             }
 
             outputStream.close();
+            inputStream.close();
+        }
+    }
+
+    /**
+     * test byte array input and output stream.
+     */
+    public void testByteArrayIO() throws Exception {
+
+        String fileUrl = conf.getProperty("test.http.input.stream.fileurl");
+        URL url = new URL(fileUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        int httpResponseCode = conn.getResponseCode();
+
+        if(httpResponseCode == 200) {
+            // opens input stream from the HTTP connection
+            InputStream inputStream = conn.getInputStream();
+            //InputStream sizeIS = inputStream.clone();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            // ======================== file size ===========================
+            // calculate the size.
+            int size = 0;
+            int chunk = 0;
+            byte[] buffer = new byte[1024];
+            while((chunk = inputStream.read(buffer)) > -1){
+                size += chunk;
+                baos.write(buffer, 0, chunk);
+            }
+            baos.flush();
+            System.out.println("file_size = " + size);
+            //System.out.println("Array Size = " + baos.toByteArray().length);
+
+            // ======================== MD5 Hash =============================
+            // generate the MD5 hash for the file content.
+            String digest = DigestUtils.md5Hex(new ByteArrayInputStream(baos.toByteArray()));
+            System.out.println("file_hash = " + digest);
+
             inputStream.close();
         }
     }
